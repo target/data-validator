3efcebee65953226ac05551dd9a08ac6e715dfa8
Colin Dean <colin.dean@target.com>
2020-02-18 15:50:37 -0500

    Refactors SumOfNumericColumn to work with substitution mechanism
    VarSub stuff requires the properties passed in to be delayed processing.
    That is, they should be strings or JSON strings until they're needed so
    that they can be substituted.
    
    This is exposing a rough edge where our check modeling is bound by the
    underlying configuration details. Instead, we should be materializing
    a check from config once the config is settled. For example,
    a CheckConfig models the YAML/JSON and then CheckConfig#buildCheck
    produces a Check without any JSON details. The CheckConfig can be
    manipulated as needed until that build executes.

--- src/main/scala/com/target/data_validator/validator/SumOfNumericColumnCheck.scala
+++ src/main/scala/com/target/data_validator/validator/SumOfNumericColumnCheck.scala
@@ -12,11 +12,21 @@ import io.circe.generic.semiauto._
 import org.apache.spark.sql.{DataFrame, Row}
 import org.apache.spark.sql.types.NumericType
 
-case class SumOfNumericColumnCheck(column: String, threshold: InclusiveThreshold)
+case class SumOfNumericColumnCheck(
+  column: String,
+  thresholdType: String,
+  threshold: Option[Json] = None,
+  lowerBound: Option[Json] = None,
+  upperBound: Option[Json] = None
+)
   // XXX: I think this condTest makes this only work on integer columns???
   extends ColumnBased(column, ValidatorBase.I0) {
 
   override def name: String = "SumOfNumericColumn"
+
+  lazy val thresholdOperator: Either[InclusiveThreshold.Error, InclusiveThreshold] = {
+    InclusiveThreshold.fromCheck(thresholdType, threshold, lowerBound, upperBound)
+  }
 
   private val sum = new AtomicInteger(0)
 
@@ -29,17 +39,20 @@ case class SumOfNumericColumnCheck(column: String, threshold: InclusiveThreshold
   override def substituteVariables(dict: VarSubstitution): ValidatorBase = {
     val ret = copy(
       column = getVarSub(column, "column", dict),
-      // FIXME: wtf lol
-      threshold = getVarSubJson(threshold.asJson, "value", dict)
-        .as[InclusiveThreshold](InclusiveThreshold.Decoder)
-        .right
-        .get
+      thresholdType = getVarSub(thresholdType, "thresholdType", dict),
+      threshold = threshold.map(getVarSubJson(_, "threshold", dict)),
+      lowerBound = lowerBound.map(getVarSubJson(_, "lowerBound", dict)),
+      upperBound = upperBound.map(getVarSubJson(_, "upperBound", dict))
     )
     this.getEvents.foreach(ret.addEvent)
     ret
   }
 
   override def configCheck(df: DataFrame): Boolean = {
+    if (thresholdOperator.isLeft) {
+      addEvent(ValidatorError(thresholdOperator.left.get.msg))
+    }
+
     findColumnInDataFrame(df, column) match {
       case Some(ft) if ft.dataType.isInstanceOf[NumericType] => Unit
       case Some(ft) =>
@@ -56,13 +69,14 @@ case class SumOfNumericColumnCheck(column: String, threshold: InclusiveThreshold
 
   override def toJson: Json = {
     val sumAtSerialization = sum.get()
-    Json.obj(
-      ("type", Json.fromString("sumOfNumericColumn")),
-      ("column", Json.fromString(column)),
-      ("sum", Json.fromInt(sumAtSerialization)),
-      ("threshold", threshold.asJson),
-      ("failed", Json.fromBoolean(threshold.isAcceptable(sumAtSerialization)))
-    )
+    val additionalFieldsForReport = Json.fromFields(Set(
+      "type" -> Json.fromString("sumOfNumericColumn"),
+      "sum" -> Json.fromInt(sumAtSerialization),
+      "failed" -> Json.fromBoolean(!thresholdOperator.right.get.isAcceptable(sumAtSerialization))
+    ))
+
+    val base = SumOfNumericColumnCheck.encoder(this)
+    base.deepMerge(additionalFieldsForReport)
   }
 }
 
@@ -75,13 +89,20 @@ object JsonConverters {
 // XXX: can we limit this to a RangedProxy?
 // TODO: genericize this
 object SumOfNumericColumnCheck extends LazyLogging {
+
+  val encoder: Encoder[SumOfNumericColumnCheck] = deriveEncoder[SumOfNumericColumnCheck]
+
+  // XXX: this looks like it could be derived
   def fromJson(c: HCursor): Either[DecodingFailure, ValidatorBase] = {
     for {
       column <- c.downField("column").as[String].right
-      threshold <- c.downField("threshold").as[InclusiveThreshold](InclusiveThreshold.Decoder).right
+      thresholdType <- c.downField("thresholdType").as[String].right
+      threshold <- c.downField("threshold").as[Option[Json]].right
+      lowerBound <- c.downField("lowerBound").as[Option[Json]].right
+      upperBound <- c.downField("upperBound").as[Option[Json]].right
     } yield {
-      logger.debug(s"Parsing SumOfNumericColumnCheck(column:$column, threshold:$threshold) config.")
-      SumOfNumericColumnCheck(column, threshold)
+      logger.debug(s"Parsing SumOfNumericColumnCheck(column:$column, threshold:$thresholdType) config.")
+      SumOfNumericColumnCheck(column, thresholdType, threshold, lowerBound, upperBound)
     }
   }
 
@@ -101,19 +122,19 @@ object SumOfNumericColumnCheck extends LazyLogging {
     def threshold: Int
 
     override def toJsonFields: Iterable[(String, String)] = List(
-      "type" -> this.getClass.getSimpleName,
+      "thresholdType" -> this.getClass.getSimpleName,
       "threshold" -> threshold.toString
     )
   }
   trait RangedInclusiveThreshold extends InclusiveThreshold {
-    def lower: Int
-    def upper: Int
-    lazy val range: Range.Inclusive = lower.to(upper)
+    def lowerBound: Int
+    def upperBound: Int
+    lazy val range: Range.Inclusive = lowerBound.to(upperBound)
 
     override def toJsonFields: Iterable[(String, String)] = List(
-      "type" -> this.getClass.getSimpleName,
-      "lower" -> lower.toString,
-      "upper" -> upper.toString
+      "thresholdType" -> this.getClass.getSimpleName,
+      "lowerBound" -> lowerBound.toString,
+      "upperBound" -> upperBound.toString
     )
   }
   case class Over(threshold: Int) extends WatermarkInclusiveThreshold {
@@ -122,14 +143,54 @@ object SumOfNumericColumnCheck extends LazyLogging {
   case class Under(threshold: Int) extends WatermarkInclusiveThreshold {
     override def isAcceptable(sum: Int): Boolean = sum >= threshold
   }
-  case class Between(lower: Int, upper: Int) extends RangedInclusiveThreshold {
+  case class Between(lowerBound: Int, upperBound: Int) extends RangedInclusiveThreshold {
     override def isAcceptable(sum: Int): Boolean = range.contains(sum)
   }
-  case class Outside(lower: Int, upper: Int) extends RangedInclusiveThreshold {
+  case class Outside(lowerBound: Int, upperBound: Int) extends RangedInclusiveThreshold {
     override def isAcceptable(sum: Int): Boolean = !range.contains(sum)
   }
 
   object InclusiveThreshold extends LazyLogging {
+
+    def fromCheck(thresholdType: String,
+                  threshold: Option[Json],
+                  lowerBound: Option[Json],
+                  upperBound: Option[Json]): Either[Error, InclusiveThreshold] = {
+      val operator = thresholdType match {
+        case "over" =>
+          optionalJsonToInt(threshold)
+            .map(Over)
+            .toRight(Error(s"The threshold required for $thresholdType was missing or invalid: [$threshold]"))
+        case "under" =>
+          optionalJsonToInt(threshold)
+            .map(Under)
+            .toRight(Error(s"The threshold required for $thresholdType was missing or invalid: [$threshold"))
+        case "between" =>
+          val op = for {
+            lower <- optionalJsonToInt(lowerBound)
+            upper <- optionalJsonToInt(upperBound)
+          } yield Between(lower, upper)
+          op.toRight(
+            Error(s"""A bound required for $thresholdType was missing or invalid: [$lowerBound -> $upperBound]"""))
+        case "outside" =>
+          val op = for {
+            lower <- optionalJsonToInt(lowerBound)
+            upper <- optionalJsonToInt(upperBound)
+          } yield Outside(lower, upper)
+          op.toRight(
+            Error(s"""A bound required for $thresholdType was missing or invalid: [$lowerBound -> $upperBound]"""))
+        case _ =>
+          Left(Error(s"""No threshold operator for type [$thresholdType]"""))
+      }
+      operator
+    }
+
+    private def optionalJsonToInt(jsonWithNumber: Option[Json]): Option[Int] = {
+      jsonWithNumber.flatMap(_.asNumber.flatMap(_.toInt))
+    }
+
+    case class Error(msg: String)
+
     object Decoder extends Decoder[InclusiveThreshold] {
       val inclusiveThresholdTypes: Map[String, Decoder[InclusiveThreshold]] = { Map(
           "over" -> deriveDecoder[Over].asInstanceOf[Decoder[InclusiveThreshold]],
@@ -140,7 +201,7 @@ object SumOfNumericColumnCheck extends LazyLogging {
       }
 
       def apply(c: HCursor): Either[DecodingFailure, InclusiveThreshold] = {
-        c.downField("type").as[String]
+        c.field("thresholdType").as[String]
           .right
           .map(inclusiveThresholdTypes.get)
           .right
--- src/test/scala/com/target/data_validator/validator/SumOfNumericColumnCheckSpec.scala
+++ src/test/scala/com/target/data_validator/validator/SumOfNumericColumnCheckSpec.scala
@@ -4,13 +4,15 @@ import com.target.TestingSparkSession
 import com.target.data_validator.TestHelpers.{mkDf, mkDict, parseYaml}
 import com.target.data_validator.validator.SumOfNumericColumnCheck._
 import com.target.data_validator.{ValidatorConfig, ValidatorDataFrame, ValidatorError}
+import io.circe._
+import io.circe.generic.semiauto._
 import org.scalatest.{FunSpec, Matchers}
 
 class SumOfNumericColumnCheckSpec extends FunSpec with Matchers with TestingSparkSession {
 
-  val expectedThreshold = 4
-  val expectedLower = 2
-  val expectedUpper = 10
+  val expectedThreshold = (4, Json.fromInt(4))
+  val expectedLower = (2, Json.fromInt(2))
+  val expectedUpper = (10, Json.fromInt(10))
 
   describe("SumOfNumericColumnCheck") {
     describe("config parsing") {
@@ -19,63 +21,64 @@ class SumOfNumericColumnCheckSpec extends FunSpec with Matchers with TestingSpar
           s"""
              |type: sumOfNumericColumnCheck
              |column: foo
-             |threshold:
-             |  type: over
-             |  threshold: ${expectedThreshold}
+             |thresholdType: over
+             |threshold: ${expectedThreshold._1}
              |""".stripMargin
         )
         val sut = JsonDecoders.decodeChecks.decodeJson(json)
-        assert(sut == Right(SumOfNumericColumnCheck("foo", Over(expectedThreshold))))
+        assert(sut == Right(
+          SumOfNumericColumnCheck("foo", "over", threshold = Some(expectedThreshold._2))))
       }
       it("uses under threshold") {
         val json = parseYaml(
           s"""
              |type: sumOfNumericColumnCheck
              |column: foo
-             |threshold:
-             |  type: under
-             |  threshold: ${expectedThreshold}
+             |thresholdType: under
+             |threshold: ${expectedThreshold._1}
              |""".stripMargin
         )
         val sut = JsonDecoders.decodeChecks.decodeJson(json)
-        assert(sut == Right(SumOfNumericColumnCheck("foo", Under(expectedThreshold))))
+        assert(sut == Right(
+          SumOfNumericColumnCheck("foo", "under", threshold = Some(expectedThreshold._2))))
       }
       it("uses between threshold") {
         val json = parseYaml(
           s"""
              |type: sumOfNumericColumnCheck
              |column: foo
-             |threshold:
-             |  type: between
-             |  lower: ${expectedLower}
-             |  upper: ${expectedUpper}
+             |thresholdType: between
+             |lowerBound: ${expectedLower._1}
+             |upperBound: ${expectedUpper._1}
              |""".stripMargin
         )
         val sut = JsonDecoders.decodeChecks.decodeJson(json)
-        assert(sut == Right(SumOfNumericColumnCheck("foo", Between(expectedLower, expectedUpper))))
+        assert(sut == Right(
+          SumOfNumericColumnCheck("foo", "between",
+            lowerBound = Some(expectedLower._2), upperBound = Some(expectedUpper._2))))
       }
       it("uses outside threshold") {
         val json = parseYaml(
           s"""
              |type: sumOfNumericColumnCheck
              |column: foo
-             |threshold:
-             |  type: outside
-             |  lower: ${expectedLower}
-             |  upper: ${expectedUpper}
+             |thresholdType: outside
+             |lowerBound: ${expectedLower._1}
+             |upperBound: ${expectedUpper._1}
              |""".stripMargin
         )
         val sut = JsonDecoders.decodeChecks.decodeJson(json)
-        assert(sut == Right(SumOfNumericColumnCheck("foo", Outside(expectedLower, expectedUpper))))
+        assert(sut == Right(
+          SumOfNumericColumnCheck("foo", "outside",
+            lowerBound = Some(expectedLower._2), upperBound = Some(expectedUpper._2))))
       }
 
       it("is missing the column") {
         val json = parseYaml(
           s"""
              |type: sumOfNumericColumnCheck
-             |threshold:
-             |  type: over
-             |  threshold: ${expectedThreshold}
+             |thresholdType: over
+             |threshold: ${expectedThreshold}
              |""".stripMargin
         )
         val sut = JsonDecoders.decodeChecks.decodeJson(json)
@@ -91,18 +94,11 @@ class SumOfNumericColumnCheckSpec extends FunSpec with Matchers with TestingSpar
         val sut = JsonDecoders.decodeChecks.decodeJson(json)
         assert(sut.isLeft)
       }
-      it("is tries to use an invalid threshold type") {
-        val json = parseYaml(
-          s"""
-             |type: sumOfNumericColumnCheck
-             |column: foo
-             |threshold:
-             |  type: overrrrrrrrrrr
-             |  threshold: ${expectedThreshold}
-             |""".stripMargin
-        )
-        val sut = JsonDecoders.decodeChecks.decodeJson(json)
-        assert(sut.isLeft)
+    }
+
+    describe("variable substitution") {
+      it("successfully substitutes") {
+        //var sut = SumOfNumericColumnCheck("$column", InclusiveThreshold(""))
       }
     }
   }
