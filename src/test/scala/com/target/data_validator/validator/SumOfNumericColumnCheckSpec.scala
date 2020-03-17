cf5a752085e61feecb9fb64821027efa542c91da
Colin Dean <colin.dean@target.com>
2020-03-17 16:05:15 -0400

    Cleans up naming in tests

--- src/test/scala/com/target/data_validator/validator/SumOfNumericColumnCheckSpec.scala
+++ src/test/scala/com/target/data_validator/validator/SumOfNumericColumnCheckSpec.scala
@@ -1,8 +1,8 @@
 package com.target.data_validator.validator
 
 import com.target.TestingSparkSession
-import com.target.data_validator.TestHelpers.{mkDf, mkDict, parseYaml}
 import com.target.data_validator.{ValidatorConfig, ValidatorDataFrame, ValidatorError}
+import com.target.data_validator.TestHelpers.{mkDf, mkDict, parseYaml}
 import io.circe._
 import org.apache.spark.sql.DataFrame
 import org.scalatest.{FunSpec, Matchers}
@@ -22,12 +22,12 @@ class SumOfNumericColumnCheckSpec
              |type: sumOfNumericColumnCheck
              |column: foo
              |thresholdType: over
-             |threshold: ${expectedThreshold._1}
+             |threshold: ${expectedThreshold8._1}
              |""".stripMargin
         )
         val sut = JsonDecoders.decodeChecks.decodeJson(json)
         assert(sut == Right(
-          SumOfNumericColumnCheck("foo", "over", threshold = Some(expectedThreshold._2))))
+          SumOfNumericColumnCheck("foo", "over", threshold = Some(expectedThreshold8._2))))
       }
       it("uses under threshold") {
         val json = parseYaml(
@@ -35,12 +35,12 @@ class SumOfNumericColumnCheckSpec
              |type: sumOfNumericColumnCheck
              |column: foo
              |thresholdType: under
-             |threshold: ${expectedThreshold._1}
+             |threshold: ${expectedThreshold8._1}
              |""".stripMargin
         )
         val sut = JsonDecoders.decodeChecks.decodeJson(json)
         assert(sut == Right(
-          SumOfNumericColumnCheck("foo", "under", threshold = Some(expectedThreshold._2))))
+          SumOfNumericColumnCheck("foo", "under", threshold = Some(expectedThreshold8._2))))
       }
       it("uses between threshold") {
         val json = parseYaml(
@@ -48,14 +48,14 @@ class SumOfNumericColumnCheckSpec
              |type: sumOfNumericColumnCheck
              |column: foo
              |thresholdType: between
-             |lowerBound: ${expectedLower._1}
-             |upperBound: ${expectedUpper._1}
+             |lowerBound: ${expectedLower2._1}
+             |upperBound: ${expectedUpper10._1}
              |""".stripMargin
         )
         val sut = JsonDecoders.decodeChecks.decodeJson(json)
         assert(sut == Right(
           SumOfNumericColumnCheck("foo", "between",
-            lowerBound = Some(expectedLower._2), upperBound = Some(expectedUpper._2))))
+            lowerBound = Some(expectedLower2._2), upperBound = Some(expectedUpper10._2))))
       }
       it("uses outside threshold") {
         val json = parseYaml(
@@ -63,14 +63,14 @@ class SumOfNumericColumnCheckSpec
              |type: sumOfNumericColumnCheck
              |column: foo
              |thresholdType: outside
-             |lowerBound: ${expectedLower._1}
-             |upperBound: ${expectedUpper._1}
+             |lowerBound: ${expectedLower2._1}
+             |upperBound: ${expectedUpper10._1}
              |""".stripMargin
         )
         val sut = JsonDecoders.decodeChecks.decodeJson(json)
         assert(sut == Right(
           SumOfNumericColumnCheck("foo", "outside",
-            lowerBound = Some(expectedLower._2), upperBound = Some(expectedUpper._2))))
+            lowerBound = Some(expectedLower2._2), upperBound = Some(expectedUpper10._2))))
       }
 
       it("is missing the column") {
@@ -78,7 +78,7 @@ class SumOfNumericColumnCheckSpec
           s"""
              |type: sumOfNumericColumnCheck
              |thresholdType: over
-             |threshold: ${expectedThreshold}
+             |threshold: ${expectedThreshold8}
              |""".stripMargin
         )
         val sut = JsonDecoders.decodeChecks.decodeJson(json)
@@ -147,19 +147,19 @@ class SumOfNumericColumnCheckSpec
     }
 
     describe("functionality") {
-      it(s"correctly checks that ${expectedThreshold._1} is not under ${listWithSum6._2.sum}") {
+      it(s"correctly checks that ${expectedThreshold8._1} is not under ${listWithSum6._2.sum}") {
         val df = mkDf(spark, listWithSum6) // scalastyle:ignore
         val sut = testDfWithChecks(df, underCheck)
         assert(!sut.quickChecks(spark, mkDict())(config))
         assert(!sut.failed)
       }
-      it(s"correctly checks that ${expectedThreshold._1} is not over ${listWithSum9._2.sum}") {
+      it(s"correctly checks that ${expectedThreshold8._1} is not over ${listWithSum9._2.sum}") {
         val df = mkDf(spark, listWithSum9) // scalastyle:ignore
         val sut = testDfWithChecks(df, overCheck)
         assert(!sut.quickChecks(spark, mkDict())(config))
         assert(!sut.failed)
       }
-      it(s"correctly checks that ${expectedThreshold._1} is over ${listWithSum6._2.sum}") {
+      it(s"correctly checks that ${expectedThreshold8._1} is over ${listWithSum6._2.sum}") {
         val df = mkDf(spark, listWithSum6) // scalastyle:ignore
         val sut = testDfWithChecks(df, overCheck)
         assert(sut.quickChecks(spark, mkDict())(config))
@@ -169,28 +169,46 @@ class SumOfNumericColumnCheckSpec
   }
 }
 trait SumOfNumericColumnCheckBasicSetup {
-  val config = ValidatorConfig(1, 1, None, false, None, None, List.empty)
+  val useDetailedErrors = false
+  val config: ValidatorConfig = ValidatorConfig(
+    numKeyCols = 1,
+    numErrorsToReport = 1,
+    email = None,
+    detailedErrors = useDetailedErrors,
+    vars = None,
+    outputs = None,
+    tables = List.empty
+  )
 
   def testDfWithChecks(df: DataFrame, checks: ValidatorBase*): ValidatorDataFrame = {
     ValidatorDataFrame(df, None, None, checks.toList)
   }
 }
-trait SumOfNumericColumnCheckExamples {
-  val expectedThreshold = (8, Json.fromInt(8))
-  val expectedLower = (2, Json.fromInt(2))
-  val expectedUpper = (10, Json.fromInt(10))
+trait SumOfNumericColumnCheckExamples extends JsonHelpers {
+  val expectedThreshold8: (Int, Json) = makeTestPair(8)
+  val expectedLower2: (Int, Json) = makeTestPair(2)
+  val expectedUpper10: (Int, Json) = makeTestPair(10)
 
-  val listWithSum6 = "price" -> List(1, 2, 3)
-  val listWithSum9 = "price" -> List(3, 3, 3)
+  val listWithSum6: (String, List[Int]) = "price" -> List(1, 2, 3)
+  val listWithSum9: (String, List[Int]) = "price" -> List(3, 3, 3)
 
   def overCheck: SumOfNumericColumnCheck =
-    SumOfNumericColumnCheck("price", "over", threshold = Some(expectedThreshold._2))
+    SumOfNumericColumnCheck("price", "over", threshold = Some(expectedThreshold8._2))
   def underCheck: SumOfNumericColumnCheck =
-    SumOfNumericColumnCheck("price", "under", threshold = Some(expectedThreshold._2))
+    SumOfNumericColumnCheck("price", "under", threshold = Some(expectedThreshold8._2))
   def betweenCheck: SumOfNumericColumnCheck =
     SumOfNumericColumnCheck("price", "between",
-    lowerBound = Some(expectedLower._2), upperBound = Some(expectedUpper._2))
+    lowerBound = Some(expectedLower2._2), upperBound = Some(expectedUpper10._2))
   def outsideCheck: SumOfNumericColumnCheck =
     SumOfNumericColumnCheck("price", "outside",
-    lowerBound = Some(expectedLower._2), upperBound = Some(expectedUpper._2))
+    lowerBound = Some(expectedLower2._2), upperBound = Some(expectedUpper10._2))
+}
+
+trait JsonHelpers {
+  /**
+    * In some parts of tests, we want an number while in others we need that number as Json
+    * @param int
+    * @return
+    */
+  def makeTestPair(int: Int): (Int, Json) = (int, Json.fromInt(int))
 }
